969d3ae333c644fea3fe446982ddc9fb364f6368
David Jungman <davidjungman.web@gmail.com>
2018-11-24 03:08:55 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,26 @@
-import RPi.GPIO as GPIO
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask.ext.jsonpify import jsonify
+import os
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+app = Flask(__name__)
+api = Api(app)
 
-print GPIO.input(4)
+
+class LightOff(Resource):
+    def get(self):
+        os.system('python LightOff.py')
+        return jsonify("success")
+
+
+class LightOn(Resource):
+    def get(self):
+        os.system('python LightOn.py')
+        return jsonify("success")
+
+
+api.add_resource(LightOff, '/api/lightoff')
+api.add_resource(LightOn, '/api/lighton')
+
+if __name__ == '__main__':
+    app.run(port='5000')
